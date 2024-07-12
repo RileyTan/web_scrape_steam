66d39ef7b30f3b22be909f71cd3a954fe5875f09
RileyTan <149601796+RileyTan@users.noreply.github.com>
2024-07-12 07:08:25 +0000

    Fri, Jul 12, 2024, 3:08 PM +08:00

--- webscraper.py
+++ webscraper.py
@@ -4,8 +4,9 @@ import lxml.html
 html = requests.get('https://store.steampowered.com/explore/new/')
 doc = lxml.html.fromstring(html.content) # equivalent of getting soup
 
-# get list of divs in the HTML page with 
+# the anchor tag of the game linking us to it's own page is encapsulated in the div with id of tab_newreleases_content. good bcos unique
 new_releases = doc.xpath('//div[@id="tab_newreleases_content"]')[0]
+
 
 
 
