bb0f39ceab4e76e7ce0a7f9c3c12019951ed228a
RileyTan <149601796+RileyTan@users.noreply.github.com>
2024-07-12 06:55:12 +0000

    Fri, Jul 12, 2024, 2:55 PM +08:00

--- webscraper.py
+++ webscraper.py
@@ -1,5 +1,6 @@
-import request
+import requests
 import lxml.html
 
 html = requests.get('https://store.steampowered.com/explore/new/')
-doc = lxml.html.fromstring(html.content)
+doc = lxml.html.fromstring(html.content) # equivalent of getting soup
+
