25005d8736f4d57abb481942a83c13b6a9f32a3e
RileyTan <149601796+RileyTan@users.noreply.github.com>
2024-07-12 07:10:33 +0000

    Fri, Jul 12, 2024, 3:10 PM +08:00

--- webscraper.py
+++ webscraper.py
@@ -6,6 +6,8 @@ doc = lxml.html.fromstring(html.content) # equivalent of getting soup
 
 # the anchor tag of the game linking us to it's own page is encapsulated in the div with id of tab_newreleases_content. good bcos unique
 new_releases = doc.xpath('//div[@id="tab_newreleases_content"]')[0]
+# new_releases contain the whole div now, [0] because only one div contains the id of tab_newreleases_content
+# // forward slashes tell lxml
 
 
 
